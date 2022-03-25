2e1a4bb440c316a15ce1b873635920f44abe596e
Mikey <mjean-baptiste@newtech.coppellisd.com>
2022-03-25 00:02:03 -0500

    math method

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -17,6 +17,20 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner scanny = new Scanner(System.in);
+		
+		System.out.println("Choose a positive integer");
+		
+		int userNom = scanny.nextInt();
+		
+		System.out.println("Choose a whole number ");
+		
+		int userNum = scanny.nextInt();
+		
+		//find power of userNom and userNum
+		
+		System.out.println(userNom + "^" + userNum + " " + Math.pow(userNom,userNum));
+		
 		
 		
 	}
@@ -26,16 +40,33 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
+		// finding users number input
 		
+		Scanner goofball = new Scanner(System.in);
+		
+		System.out.println("Choose a positive integer");
+		
+		int userDum = goofball.nextInt();
+		
+		System.out.println(" The square root of " + userDum + " is " + Math.sqrt(userDum));
 		
 	}
-
 	
 	
 
 	public static void problem03() {
 		
+		Scanner poop = new Scanner(System.in);
 		
+		System.out.println("What is side 1 of a Right Triangle");
+		
+		int userDumb = poop.nextInt();
+		
+		System.out.println("What is side 2 of a Right Triangle");
+		
+		int userAlsoDumb = poop.nextInt();
+		
+		System.out.println("Hypotenuse = " + Math.sqrt(Math.pow(userDumb, 2 )+(Math.pow(userAlsoDumb, 2))));
 		
 
 	}
@@ -44,6 +75,35 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
+		// maximum and minimum values 
+		
+		int maximom = Integer.MIN_VALUE;
+		int minimom = Integer.MAX_VALUE;
+		Scanner goop = new Scanner(System.in);
+		int userBum = 1;
+		boolean notZer0 = true;
+		
+		while (notZer0) {
+			System.out.println(" Choose an integer, afterwards choose 0");
+			userBum = goop.nextInt();
+			if (userBum == 0) {
+				notZer0 = false; 
+			
+						
+			}
+			else {		
+					if (userBum > maximom) {
+						maximom = userBum;
+					
+					}
+					else if (userBum< minimom ) {
+						minimom = userBum;
+						
+					}
+			}
+			}
+			
+		}
 		
 		
 		
@@ -51,7 +111,6 @@ public class Lab01 {
 	
 	
 	
+
 	
-	
-	
-}
+
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -17,6 +17,37 @@ public class Lab02 {
 	
 	public static void problem01() {
 		
+		Scanner binkey = new Scanner(System.in);
+		
+		System.out.println("Choose a positive integer");
+		
+		int userBum = binkey.nextInt();
+		
+		System.out.println("Choose Another Positive Integer ");
+		
+		int userDumb2 = binkey.nextInt();
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		int x1 = userBum;
+		int x2 = userDumb2;
+		
+		max = Math.max(max, x1);
+		max = Math.max(max, x2);
+		min = Math.min(min, x1);
+		min = Math.min(min, x2);
+
+		
+		int random = max - min + 1;
+		int random2 = min;
+		
+		for (int i = 0; i < 10; i++) {
+			int computer = (int) (Math.random() * random + random2 );
+			System.out.println(computer + " ");
+		}
+		
+			System.out.println(" ");
 		
 		
 		
@@ -27,6 +58,17 @@ public class Lab02 {
 	
 	public static void problem02() {
 		
+		Scanner binkey = new Scanner(System.in);
+
+		System.out.println("Give Me The Radius And Height Of A Cylinder");
+
+		int userInput = binkey.nextInt();
+		
+	
+		int Input1 = userInput;
+		
+		
+
 		
 		
 	}
